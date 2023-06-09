9d76d6eecf61ab634a674044869abaea5a586db8
ramyohan90 <ram2612199@gmail.com>
2023-06-09 23:47:22 +0530

    Tailwind configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 
-import './styles/style.css'
+import './styles/styles.css'
+
 export function App() {
     return (
         <div>
-            <h1>
-                Welcome Sats community
+            <h1 className='text-green-600'>
+                Welcome Sats community - React + Tailwind
             </h1>
         </div>
     )
