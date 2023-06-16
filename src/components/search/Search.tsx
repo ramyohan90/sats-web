beb9d97949c3f96969cfd1c39d25362416439fca
ramyohan90 <ram2612199@gmail.com>
2023-06-16 17:44:36 +0530

    search feature and top header made fixed.

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -5,7 +5,7 @@ export function SearchComponent() {
             <div className="">
                 <div>
                     <input autoComplete="off" type="text" placeholder="Search Inscription, address, brc20.."
-                        className="p-2 rounded xs:w-full xxs:w-full sm:w-full md:w-full lg:w-1/2 2xl:w-1/2 focus:ring focus:ring-satsColor focus:outline-none" />
+                        className="p-2 rounded xs:w-full xxs:w-full sm:w-full border border-satsColor md:w-full lg:w-1/2 2xl:w-1/2 focus:ring focus:ring-satsColor focus:outline-none" />
                         <div className="inline-block bg-satsColor rounded md:ml-1 px-6 py-2 xs:mt-2 sm:mt-2 md:mt-0">
                             Search
                         </div>
